ac18cef30a64e8f52695156f51b9010235c94cf5
Allen <ae82192@uga.edu>
2024-11-21 21:28:34 -0500

    Header
    Added drop down menu with genres

--- my-app/src/app/Home_Page/Header.tsx
+++ my-app/src/app/Home_Page/Header.tsx
@@ -1,29 +1,48 @@
-/* CONSISTS OF DROPDOWN MENU, PROSE TITLE, LOGIN, REGISTER ON HOME PAGE */
+"use client";
 
+import React, { useState } from 'react';
 import './Header.css';
 import Button from '../Shared_Components/Button';
-import {IM_Fell_English_SC} from 'next/font/google';
-import GenreButton from './GenreButton';
+import { IM_Fell_English_SC } from 'next/font/google';
+import GenreButton from './GenreButton'; // This might already contain the big burger icon
 
 const titleFont = IM_Fell_English_SC({
-    subsets: ['latin'],
-    weight: ['400'],
-    adjustFontFallback: false,
-})
+  subsets: ['latin'],
+  weight: ['400'],
+  adjustFontFallback: false,
+});
 
-export default function Header() 
-{
-    return (
-        <header >
-            <div className='title-container'>
-                <GenreButton/>
-                <h1 className={`${titleFont.className} title`}>Prose</h1>
-            </div>
+export default function Header() {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const genres = ['Fantasy', 'Sci-Fi', 'Mystery', 'Non-Fiction', 'Romance', 'Young Adult', 'Education', 'Other'];
 
-            <div>
-                <Button text="Login" targetPage='../Login_Page'/>
-                <Button text="Register" targetPage='../Register_Page'/>
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
+    <header>
+      <div className="title-container">
+        {/* Use existing burger icon for the dropdown */}
+        <div onClick={toggleDropdown}>
+          <GenreButton />
+        </div>
+        <h1 className={`${titleFont.className} title`}>Prose</h1>
+      </div>
+      {/* Dropdown Menu */}
+      {isDropdownOpen && (
+        <div className="dropdown-menu">
+          {genres.map((genre, index) => (
+            <div className="dropdown-item" key={index}>
+              {genre}
             </div>
-        </header>
-    );
-};
+          ))}
+        </div>
+      )}
+      <div>
+        <Button text="Login" targetPage="../Login_Page" />
+        <Button text="Register" targetPage="../Register_Page" />
+      </div>
+    </header>
+  );
+}
